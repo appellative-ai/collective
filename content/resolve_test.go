a4fdde6cae7b2adc4512365da735ff70bba133e2
advanced-go <mark.wilson.go@outlook.com>
2025-03-13 04:53:46 -0500

    Update resolve_test.go

--- content/resolve_test.go
+++ content/resolve_test.go
@@ -1,1 +1,23 @@
 package content
+
+import (
+	"fmt"
+	"github.com/behavioral-ai/collective/testrsc"
+	url2 "net/url"
+)
+
+func ExampleResolution_PutValue() {
+	urn := "type/test"
+	r := NewEphemeralResolver()
+	url, _ := url2.Parse(testrsc.ResiliencyTrafficProfile1)
+
+	status := r.PutValue(urn, "author", url, 1)
+	fmt.Printf("test: PutValue() -> [status:%v]\n", status)
+
+	buf, status1 := r.GetValue(urn, 1)
+	fmt.Printf("test: GetValue() -> [status:%v] [%v]\n", status1, string(buf))
+
+	//Output:
+	//fail
+
+}
