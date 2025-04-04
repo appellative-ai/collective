3e43f0a5cd3f389f3f5bfa2cd376c0862cc110b3
advanced-go <mark.wilson.go@outlook.com>
2025-04-04 11:15:13 -0500

    Update message.go

--- timeseries/message.go
+++ timeseries/message.go
@@ -7,17 +7,17 @@ const (
 	ContentTypeEvent = "application/event"
 )
 
-func NewLoadMessage(e []Event) *messaging.Message {
+func NewLoadMessage(e []*Event) *messaging.Message {
 	m := messaging.NewMessage(messaging.Control, LoadEvent)
 	m.SetContent(ContentTypeEvent, e)
 	return m
 }
 
-func LoadContent(m *messaging.Message) []Event {
+func LoadContent(m *messaging.Message) []*Event {
 	if m.Event() != LoadEvent || m.ContentType() != ContentTypeEvent {
 		return nil
 	}
-	if e, ok := m.Body.([]Event); ok {
+	if e, ok := m.Body.([]*Event); ok {
 		return e
 	}
 	return nil
