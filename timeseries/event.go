a33cb5d237f8fdfb7480957a86061df11449c3ec
advanced-go <mark.wilson.go@outlook.com>
2025-04-08 14:31:38 -0500

    Adding path to event

--- timeseries/event.go
+++ timeseries/event.go
@@ -4,6 +4,7 @@ import "time"
 
 type Event struct {
 	Origin     Origin        `json:"origin"`
+	Path       string        `json:"path"` // uri path
 	Start      time.Time     `json:"start-ts"`
 	Duration   time.Duration `json:"duration"`
 	StatusCode int           `json:"status-code"`
