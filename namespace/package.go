36e95e864d549deef92fa1a9d0580008260f822a
advanced-go <mark.wilson.go@outlook.com>
2025-05-10 10:34:43 -0500

    Update package.go

--- namespace/package.go
+++ namespace/package.go
@@ -4,10 +4,27 @@ import (
 	"github.com/behavioral-ai/core/messaging"
 )
 
+// TODO : add activity
+//        Can we add frame content version to activity?
+//        Need author, frame, things, Accessor
+//        Need counts, lists of aspects
+
+// Accessor -
+type Accessor struct {
+	//Version string // returned on a Get
+	Type    string // Content-Type
+	Content any
+}
+
 // Adder - add
 type Adder struct {
 	Thing    func(name, cname, author string) *messaging.Status
 	Relation func(name, cname, thing1, thing2, author, authority string) *messaging.Status
+	// What exactly are the results?
+	// How to query+select/return generational information
+	// Content can be captured if provided.
+	ConnectThing  func(name, frame, author string, access Accessor) (results string, status *messaging.Status)
+	ConnectAspect func(name []string, frame, author string, access Accessor) (results string, status *messaging.Status)
 }
 
 // Add -
