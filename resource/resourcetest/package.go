7ceaa1872458e66667968eb46387a40b8e1ac75f
advanced-go <mark.wilson.go@outlook.com>
2025-06-01 10:45:12 -0500

    Adding test resolver

---
+++ resource/resourcetest/package.go
@@ -0,0 +1,30 @@
+package resourcetest
+
+import (
+	"github.com/behavioral-ai/collective/repository"
+	"github.com/behavioral-ai/collective/resource"
+	"github.com/behavioral-ai/core/messaging"
+)
+
+var Resolver = func() *resource.Resolution {
+	return &resource.Resolution{
+		Representation: func(name, fragment string) (resource.Content, *messaging.Status) {
+			return resource.Content{}, messaging.StatusOK()
+		},
+		AddRepresentation: func(name, fragment, author string, value any) *messaging.Status {
+			return messaging.StatusOK()
+		},
+		Context: func(name string) (resource.Content, *messaging.Status) {
+			return resource.Content{}, messaging.StatusOK()
+		},
+		AddContext: func(name, author string, ct resource.Content) *messaging.Status {
+			return messaging.StatusOK()
+		},
+		AddTrace: func(name, task, observation, action string) *messaging.Status {
+			o := repository.GetOrigin()
+			if o.Host == "" {
+			}
+			return messaging.StatusOK()
+		},
+	}
+}()
