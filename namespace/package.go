9ca9efa5ad83d0bb6971c6ddf687c49038023350
advanced-go <mark.wilson.go@outlook.com>
2025-07-09 13:28:32 -0500

    Update package.go

--- namespace/package.go
+++ namespace/package.go
@@ -33,9 +33,22 @@ const (
 // What is a name? -> a word or phrase that constitutes the distinctive designation of a person or thing
 //
 
+// How to support temporal ordering - order, on a join??
+// We could add an aspect to the join, which would create a one-to-one relationship between the two things
+// and an aspect. To implement with the current architecture would require to create a unique aspect every time
+// and ordering is created. Maybe too much overhead?? Also, the created unique order aspect would be ignored by all
+// attempts/queries to determine relationships.
+// If this is an infrequently used functionality, maybe the creation of a unique order aspect is not too much overhead.
+// However, if there is other aspects that need to be used, then it becomes harder to justify the overhead.
+// But, to join 2 things on an aspect other than order, the normal mechanism would be used.
+// So, maybe there is an order join?
+// The things in an order join would be constrained to: perceptions, observations, events.
+// So, if a perception is an aspect will that work?
+// Probably not limit thing1, and thing2, but the aspect would always be core:common:order
+
 const (
 	ThingKind = "thing" // A name
-	JoinKind  = "join"  // Join 2 things
+	JoinKind  = "join"  // Join 2, maybe 3 things
 	FrameKind = "frame" // A container for things, similar to a dir entry on a file system
 
 	// AspectKind - A way in which something can be viewed by the mind. Aspects vary from real
